053066149b6d85e435abcabc0f55bd323aef7984
tschumann <schumann.tom@gmail.com>
2017-05-13 13:20:53 +1000

    Updated scripts.

--- build.bat
+++
@@ -1,4 +0,0 @@
-cd src/dcmpl
-:: the Go compiler complains if there are .c files
-rm *.c
-C:\Go\bin\go build
--- test1.bat
+++ test1.bat
@@ -1,3 +1,2 @@
-call build.bat
-dcmpl.exe PF_precache_model_I.asm
-type PF_precache_model_I.asm.c
+C:\Users\schum\AppData\Local\Programs\Python\Python36\python src\dcmpl\dcmpl.py src\dcmpl\PF_precache_model_I.asm
+type src\dcmpl\PF_precache_model_I.asm.c
