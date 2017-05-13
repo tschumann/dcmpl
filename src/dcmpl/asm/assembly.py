0db537f476de55d4a24cd2e554e6b71fe11d0f01
tschumann <schumann.tom@gmail.com>
2017-05-13 13:24:38 +1000

    Got rid of more Go.

--- src/dcmpl/asm/assembly.py
+++ src/dcmpl/asm/assembly.py
@@ -1,1 +1,8 @@
 import asm.x86.x86_instruction
+
+class Line(object):
+	instruction = None
+	arguments = []
+	
+class Assembly(object):
+	lines = []
--- test1.bat
+++ test1.bat
@@ -1,2 +1,3 @@
+rm src\dcmpl\PF_precache_model_I.asm.c
 C:\Users\schum\AppData\Local\Programs\Python\Python36\python src\dcmpl\dcmpl.py src\dcmpl\PF_precache_model_I.asm
 type src\dcmpl\PF_precache_model_I.asm.c
