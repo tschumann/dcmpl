3299bd09802ef31dca8de0dd0a9fbd89963a703f
tschumann <schumann.tom@gmail.com>
2017-05-13 13:06:19 +1000

    Moving to Python instead of battling Go at every step of the way.

--- src/dcmpl/dcmpl.py
+++ src/dcmpl/dcmpl.py
@@ -1,9 +1,56 @@
 import asm.instruction
 import asm.assembly
+import re
 import sys
 
 def decompile(filename):
-	print('test')
+	raw_lines = []
+	cleaned_lines = []
+	
+	with open(filename) as file:
+		raw_lines = file.read().splitlines()
+		
+	for line in raw_lines:
+		tokens = line.split()
+		cleaned_line = []
+
+		for token in tokens:
+			# skip comments
+			if token[0] == ";":
+				continue
+				
+			if re.match("\\.\\w+:\\d+", token):
+				continue
+
+			if token[-1:] == ",":
+				token = token[:-1]
+				
+			cleaned_line.append(token)
+			
+		if len(cleaned_line) > 0:
+			cleaned_lines.append(cleaned_line)
+			
+	output_file = open(filename + ".c", "w")
+			
+	for line in cleaned_lines:
+		tokens = line
+		for index in range(0, len(line)):
+			# if it's the first token then it could be an instruction
+			# if it doesn't end with a : then it is an instruction
+			if index == 0 and tokens[0][-1:] != ":":
+				if tokens[0] == "push":
+					pass
+				elif tokens[0] == "pop":
+					pass
+				elif tokens[0] == "mov":
+					if tokens[2] == "offset":
+						output_file.write(tokens[1] + " = &" + tokens[3] + ";\n")
+					elif tokens[1] == "dword" and len(tokens) > 3 and tokens[2] == "ptr":
+						output_file.write(tokens[3] + " = " + tokens[4] + ";\n")
+					else:
+						output_file.write(tokens[1] + " = " + tokens[2] + ";\n")
+					continue
+				
 
 if __name__ == '__main__':
 	if len(sys.argv) < 2:
